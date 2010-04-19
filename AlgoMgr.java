0ed5349153d398ca662afc502347ea2f2a1c4212
devicenull <dn@devicenull.org>
2010-04-19 17:03:51 -0400

    Cleaned up formatting in AlgoMgr

--- AlgoMgr.java
+++ AlgoMgr.java
@@ -2,33 +2,23 @@ import java.util.ArrayList;
 
 public class AlgoMgr {
 	ArrayList<Algorithm> loaded = new ArrayList<Algorithm>();
-	
-	public void Load(String s)
-	{
-		Log.info("Loading algorithm "+s);
-		try{
+
+	public void Load(String s) {
+		Log.info("Loading algorithm " + s);
+		try {
 			Class<?> cAlgo = Class.forName(s);
-			Algorithm algo = (Algorithm)cAlgo.newInstance();
+			Algorithm algo = (Algorithm) cAlgo.newInstance();
 			loaded.add(algo);
+		} catch (ClassNotFoundException e) {
+			Log.error("Couldn't find algorith class " + s);
+		} catch (IllegalAccessException e) {
+			Log.error("IllegalAccessException while trying to create " + s);
+		} catch (InstantiationException e) {
+			Log.error("InstantionException while trying to create " + s);
 		}
-		catch (ClassNotFoundException e)
-		{
-			Log.error("Couldn't find algorith class "+s);
-		}
-		catch (IllegalAccessException e)
-		{
-			Log.error("IllegalAccessException while trying to create "+s);
-		}
-		catch (InstantiationException e)
-		{
-			Log.error("InstantionException while trying to create "+s);
-		}
-		
-		
 	}
-	
-	public Algorithm[] getAlgorithms()
-	{
+
+	public Algorithm[] getAlgorithms() {
 		Algorithm ret[] = new Algorithm[loaded.size()];
 		loaded.toArray(ret);
 		return ret;
